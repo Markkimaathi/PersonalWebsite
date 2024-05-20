5b55ea26a7511bfab883fe8414d98f2f7025910a
Mark Kimathi <mark.kimathi@technobraingroup.com>
2024-05-20 11:51:46 +0300

    experience added

---
+++ src/components/Experience.js
@@ -0,0 +1,34 @@
+import React from "react";
+import JobList from "./JobList";
+import "../styles/Experience.css";
+import FadeInSection from "./FadeInSection";
+
+class Experience extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      expanded: true,
+      activeKey: "1"
+    };
+    this.handleSelect = this.handleSelect.bind(this);
+  }
+  handleSelect(eventKey) {
+    this.setState({
+      activeKey: eventKey
+    });
+  }
+  render() {
+    return (
+      <div id="experience">
+        <FadeInSection>
+          <div className="section-header ">
+            <span className="section-title">/ experience</span>
+          </div>
+          <JobList></JobList>
+        </FadeInSection>
+      </div>
+    );
+  }
+}
+
+export default Experience;
