20a414f3db1181ede532eb980601beccb841db82
Mark Kimathi <mark.kimathi@technobraingroup.com>
2024-05-20 12:18:31 +0300

    navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,42 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import EmailRoundedIcon from "@material-ui/icons/EmailRounded";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import "../styles/NavBar.css";
+
+class NavBar extends React.Component {
+  render() {
+    return (
+      <Navbar fixed="top" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="#">Mark Kimathi</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#intro">Home</Nav.Link>
+              <Nav.Link href="#about">About</Nav.Link>
+              <Nav.Link href="#experience">Experience</Nav.Link>
+              <Nav.Link href="#projects">Projects</Nav.Link>
+            </Nav>
+            <Nav className="ml-auto">
+              <Nav.Link href="mailto:markangelkimathi@gmail.com">
+                <EmailRoundedIcon style={{ fontSize: 20 }}></EmailRoundedIcon>
+              </Nav.Link>
+              <Nav.Link href="https://github.com/Markkimaathi/" target="_blank">
+                <GitHubIcon style={{ fontSize: 19 }}></GitHubIcon>
+              </Nav.Link>
+              <Nav.Link href="https://www.linkedin.com/in/mark-kimathi-58257b305/" target="_blank">
+                <LinkedInIcon style={{ fontSize: 21 }}></LinkedInIcon>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    );
+  }
+}
+
+export default NavBar;
