eb692eb1d13193aa161de151b36258c4896b2acc
Mark Kimathi <mark.kimathi@technobraingroup.com>
2024-05-20 12:00:14 +0300

    intro

---
+++ src/components/Intro.js
@@ -0,0 +1,56 @@
+import React from "react";
+
+import "../styles/Intro.css";
+import Typist from "react-typist";
+import "react-typist/dist/Typist.css";
+import EmailRoundedIcon from "@material-ui/icons/EmailRounded";
+import FadeInSection from "./FadeInSection";
+import FractalTree from "./FractalTree";
+
+class Intro extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      expanded: true,
+      activeKey: "1",
+      visible: true,
+    };
+    this.handleSelect = this.handleSelect.bind(this);
+  }
+  handleSelect(eventKey) {
+    this.setState({
+      activeKey: eventKey,
+    });
+  }
+  render() {
+    return (
+      <div id="intro">
+        <FractalTree></FractalTree>
+        <Typist avgTypingDelay={120}>
+          <span className="intro-title">
+            {"hi, "}
+            <span className="intro-name">{"gazi"}</span>
+            {" here."}
+          </span>
+        </Typist>
+        <FadeInSection>
+          <div className="intro-subtitle">I create stuff sometimes.</div>
+          <div className="intro-desc">
+          I am Full Stack Development Intern hailing from the bustling tech scene of Nairobi, Kenya. 
+          With a keen eye for detail and a passion for innovation,
+          I am driven by the challenge of creating impactful digital solutions. 
+          </div>
+          <a
+            href="mailto:markangelkimathi@gmail.com"
+            className="intro-contact"
+          >
+            <EmailRoundedIcon></EmailRoundedIcon>
+            {" Say hi!"}
+          </a>
+        </FadeInSection>
+      </div>
+    );
+  }
+}
+
+export default Intro;
